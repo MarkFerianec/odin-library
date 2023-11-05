ae3e616abb1e635b7f05a1d087e41a6448983e31
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-11-05 09:16:05 -0700

    Modify script
    Refactored Book function into class.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
+class Book {
+
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
         let bookReadOrNot;
         if (read == true) {
             bookReadOrNot = "read";
@@ -14,9 +18,26 @@ function Book(title, author, pages, read) {
             bookReadOrNot = "not read yet";
         }
         return title + " by " + author + ", " + pages + " pages, " + bookReadOrNot;
-        // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
     }
 }
+
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function () {
+//         let bookReadOrNot;
+//         if (read == true) {
+//             bookReadOrNot = "read";
+//         }
+//         else {
+//             bookReadOrNot = "not read yet";
+//         }
+//         return title + " by " + author + ", " + pages + " pages, " + bookReadOrNot;
+//         // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+//     }
+// }
 
 // hobbit = new Book("Hobbit", "Tolkien", 295, true);
 
